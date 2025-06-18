732c3d6876e00ff2f0a1b5ae3bc951e73767e684
Victor Benetti <b3n3771@hotmail.com>
2025-06-17 22:26:20 -0300

    report shows seller name

--- src/main/java/com/devsuperior/dsmeta/dto/SaleMinDTO.java
+++ src/main/java/com/devsuperior/dsmeta/dto/SaleMinDTO.java
@@ -9,17 +9,20 @@ public class SaleMinDTO {
 	private Long id;
 	private Double amount;
 	private LocalDate date;
+	private String sellerId;
 	
-	public SaleMinDTO(Long id, Double amount, LocalDate date) {
+	public SaleMinDTO(Long id, Double amount, LocalDate date, String sellerId) {
 		this.id = id;
 		this.amount = amount;
 		this.date = date;
+		this.sellerId = sellerId;
 	}
 	
 	public SaleMinDTO(Sale entity) {
 		id = entity.getId();
 		amount = entity.getAmount();
 		date = entity.getDate();
+		sellerId = entity.getSeller().getName();
 	}
 
 	public Long getId() {
@@ -32,5 +35,9 @@ public class SaleMinDTO {
 
 	public LocalDate getDate() {
 		return date;
+	}
+
+	public String getSellerId() {
+		return sellerId;
 	}
 }
